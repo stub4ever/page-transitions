18fce878412ea50c7c900c7130dc1c5d3db61147
quoc <bui.vquoc@gmail.com>
2019-11-18 11:14:04 +0700

    Correcting our navigation links

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -17,7 +17,28 @@ barba.init({
     {
       // css classes will look like `.fade-xxx-[-xxx]`
       name: 'anim-fade',
-      beforeEnter() {
+      // Data argument is an object passed to all transition hooks, view hooks subset and custom rules
+      // eslint-disable-next-line no-unused-vars
+      beforeEnter({ current, next, trigger }) { // Passed barba data agrument
+        // Current - Current page related
+        // Next - Next page related
+        // Trigger - Link that triggered the transition
+
+        // Get 3 current navlinks properties
+        const headerLinks = document.querySelectorAll('header a');
+        // Barba provide data agrument to get properties url object
+        const hrefNextURLPath = next.url.path; // Getting the next page url path
+
+        headerLinks.forEach((link) => {
+          // Check if the next page link is equal to the right page
+          // add selected class or remove
+          if (link.getAttribute('href') === hrefNextURLPath) {
+            link.classList.add('selected');
+          } else {
+            link.classList.remove('selected'); // clear all links
+          }
+        });
+
         // Add hooks before user go through other page scroll back to top => Prevent to stuck in the middle
         window.scrollTo({
           top: 0, // scroll to top 0 in a smooth way
@@ -27,3 +48,4 @@ barba.init({
     },
   ],
 });
+
