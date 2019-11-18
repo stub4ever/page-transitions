948b9b04f9e9b13e8c1bfe4357c1a370a29100cb
quoc <bui.vquoc@gmail.com>
2019-11-18 10:29:03 +0700

    Barba's beforeEnter hook

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // require('./polyfill');
 import Util from './utils/Util';
 // import Resizer from "./utils/Resizer";
@@ -16,6 +17,13 @@ barba.init({
     {
       // css classes will look like `.fade-xxx-[-xxx]`
       name: 'anim-fade',
+      beforeEnter() {
+        // Add hooks before user go through other page scroll back to top => Prevent to stuck in the middle
+        window.scrollTo({
+          top: 0, // scroll to top 0 in a smooth way
+          behavior: 'smooth',
+        });
+      },
     },
   ],
 });
